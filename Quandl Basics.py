b224e5a84326342a160589fccfe91bab73c0109a
ChristopherLam33 <lam133@purdue.edu>
2022-05-11 21:49:51 -0400

    Quandl + .csv files using pandas

--- Quandl Basics.py
+++ Quandl Basics.py
@@ -5,6 +5,9 @@ import numpy as np
 
 qd.ApiConfig.api_key = "DbJxfq6gcxYt6NXAZVy6"
 
+## WTI Crude Oil Prices
+print("\nWTI Crude Oil Prices\n")
+
 # Std display
 my_data = qd.get("EIA/PET_RWTC_D")
 print(my_data)
@@ -13,3 +16,10 @@ print(my_data)
 my_data = qd.get("EIA/PET_RWTC_D", returns="numpy")
 print(my_data)
 
+## US GDP Data
+print("\nUS GDP Data\n")
+my_data = qd.get("FRED/GDP", start_date="2019-12-31", end_date="2020-9-26")
+print(my_data)
+
+df = pd.DataFrame(my_data, columns=["Date", "Value"])
+df.to_csv("GDP.csv")
